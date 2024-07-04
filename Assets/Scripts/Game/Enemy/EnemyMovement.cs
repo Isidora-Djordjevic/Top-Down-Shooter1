d161bf1ac16d4bacda5bb40aa34a227ca83ecd2c
andjelacv <andjela.cvijanovic16@gmail.com>
2024-07-04 15:25:38 +0200

    making the enemies change direction randomly

--- Assets/Scripts/Game/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -14,13 +14,15 @@ public class EnemyMovement : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private PlayerAwarenessController _playerAwarenessController;
     private Vector2 _targetDirection;
+    private float _changeDirectionCoolDown;
 
 
     // Start is called before the first frame update
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _playerAwarenessController = GetComponent<PlayerAwarenessController>(); 
+        _playerAwarenessController = GetComponent<PlayerAwarenessController>();
+        _targetDirection = transform.up;
 
     }
 
@@ -35,23 +37,39 @@ public class EnemyMovement : MonoBehaviour
     //postavljamo pravac kretanja
     private void UpdateTargetDirection()
     {
-        if(_playerAwarenessController.AwareOfPlayer)
+        HandleRandomDirectionChange();
+        HandlePlayerTargeting();
+       
+       
+    }
+
+
+    private void HandleRandomDirectionChange()
+    {
+        _changeDirectionCoolDown -= Time.deltaTime;
+        if (_changeDirectionCoolDown <= 0)
+        {
+            float angleChange=Random.Range(-90f,+90f);
+            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
+            _targetDirection = rotation * _targetDirection;
+            _changeDirectionCoolDown = Random.Range(1f, 5f);
+        }
+
+    }
+    private void HandlePlayerTargeting()
+    {
+
+        if (_playerAwarenessController.AwareOfPlayer)
         {
             _targetDirection = _playerAwarenessController.DirectionToPlayer;
         }
-        else
-        {
-            _targetDirection = Vector2.zero;
-        }
+
     }
 
     //delta time se koristi da bi se okretao istom brzinom nezavisno od FrameRate-a
     private void RotateTowardsTarget()
     {
-        if (_targetDirection == Vector2.zero)
-        {
-            return;
-        }
+        
 
         float angle = Mathf.Atan2(_targetDirection.y, _targetDirection.x) * Mathf.Rad2Deg - 90f;
         float rotation = Mathf.LerpAngle(_rigidbody.rotation, angle, _rotationSpeed * Time.deltaTime);
@@ -62,13 +80,9 @@ public class EnemyMovement : MonoBehaviour
 
     private void SetVelocity()
     {
-        if(_targetDirection == Vector2.zero)
-        {
-            _rigidbody.velocity = Vector2.zero;
-        }
-        else
-        {
+      
+        
             _rigidbody.velocity = transform.up * _speed;
-        }
+        
     }
 }
